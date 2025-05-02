d0e0e61d9c28c00f2838cf72abc092b944446c28
Flopster101 <nahuelgomez329@gmail.com>
2025-05-02 04:01:37 -0300

    BoardConfig: move version setting to common tree
    The device name will be passed to the common tree, which will set TW_DEVICE_VERSION.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,11 +18,13 @@ DEVICE_PATH := device/samsung/a25x
 
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/recovery_dtbo
 
+DEVICE_CODENAME := a25x
+
 # Inherit from common tree
 include device/samsung/s5e8825-common/BoardConfigCommon.mk
 
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # TWRP specific build flags
-TW_DEVICE_VERSION := Samsung Galaxy A25 5G
+# TW_DEVICE_VERSION := Samsung Galaxy A25 5G
 TW_FRAMERATE := 120
